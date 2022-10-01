d570c3e19e4717bc614995aea5a43c0ce5140ffe
MINJOO-KIM <gkminju2000@gmail.com>
2022-10-02 00:30:25 +0900

    feat: 모바일 상단 이미지를 설정한다

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const faqCheckboxes = document.querySelectorAll(".faq-checkbox");
 const answers = document.querySelectorAll(".answer");
 const questions = document.querySelectorAll(".question");
+const arrowImages = document.querySelectorAll(".question > img");
 
 faqCheckboxes.forEach(function (faqCheckbox, index) {
   faqCheckbox.addEventListener("change", function () {
     if (this.checked) {
       answers[index].classList.remove("hidden");
       questions[index].style.fontWeight = 700;
+      questions[index].style.color = "hsl(237, 12%, 33%)";
+      arrowImages[index].style.transform = "rotate(180deg)";
     } else {
       answers[index].classList.add("hidden");
-      questions[index].style.fontWeight = 400;
+      questions[index].style.fontWeight = "";
+      questions[index].style.color = "";
+      arrowImages[index].style.transform = "rotate(0deg)";
     }
   });
 });
